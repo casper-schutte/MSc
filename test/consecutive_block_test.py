104a7b1527d68b428b8a3156c2865cbed0e6b65f
casper <casperjhs@gmail.com>
2021-11-05 16:46:56 +0200

    Update 5 Nov

--- test/consecutive_block_test.py
+++ test/consecutive_block_test.py
@@ -3,12 +3,15 @@ import numpy as np
 from class_SAM import SAM
 
 file = SAM()
-filepath = 'chrom_test.sam'
+filepath = 'exp5_rev3_re.sam'
 number = file.ReadSAMFile(filepath)
 
 my_array = []
 read_nums = []
 my_chroms = []
+my_as = []
+my_list = []
+my_score = []
 
 
 def find_consecutive_blocks():
@@ -16,23 +19,46 @@ def find_consecutive_blocks():
         my_array.append(int(file.GetField(i, "POS")))
         read_nums.append(file.GetField(i, "QNAME"))
         my_chroms.append((file.GetField(i, "RNAME")))
+        my_as.append(file.GetOPTValue(i, "AS"))
+        my_score.append(file.GetField(i, "MAPQ"))
     diffs = np.diff(my_array)
     iterable = []
     borders = []
     pos = 0
-    print(diffs)
+    last_pos = 0
     while pos + 1 < len(my_array):
-        if diffs[pos] > 100 or diffs[pos] < 0:
-            iterable.append([abc(pos), abc(pos + 1)])
+        if diffs[pos] > 100 or diffs[pos] < -150 or diffs[pos] == 1:
+            # These numbers are set according to how the reads are simultaneously created from the end
+            # and the start of the file. Sometimes an intermediate read is created which overlaps the middle
+            # and this can cause the program to question the continuity of the reads. The allowance of
+            # overlap solves this.
+            # The last OR statement lets us catch insertions at the start of a sequence, because changing from
+            # position 0 (didn't map anywhere) to position 1 (start of sequence) does not trigger the previous
+            # arguements.
+            iterable.append([[abc(last_pos), abc(pos)]])
+            last_pos = pos + 1
         pos += 1
+    iterable.append([[abc(last_pos), abc(pos)]])
+
     print(iterable)
 
     flat_list = [y for z in iterable for y in z]
-    print(flat_list)
+
+    for border in flat_list:
+        print(border)
+
+    # print(flat_list[0][0][0])
+
+    # return flat_list
+
+    # there is a way to return the order as well, using the pos and arranging them consecutively.
+    # One can also tell if there was a reversal because the positions will be reversed (IF they map
+    # at all).
 
 
 def abc(i):
-    return [my_array[i], read_nums[i], my_chroms[i]]
+    # return [my_array[i], read_nums[i], my_chroms[i], my_as[i]]
+    return my_array[i], read_nums[i], my_score[i]
 
 
 if __name__ == "__main__":
