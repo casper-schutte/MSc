7e23b0c5d54574aa5facaf36a5adbac32a62e46b
casper <casperjhs@gmail.com>
2021-11-18 17:47:32 +0200

    Script was changed and now identifies borders. Borders are reported multiple time from different reads or multiple possible breakpoints are reported in close proximity

--- test/find_borders_test.py
+++ test/find_borders_test.py
@@ -3,11 +3,18 @@ import numpy as np
 from class_SAM import SAM
 
 file = SAM()
-filepath = 'exp7_1.sam'
+filepath = 'exp7_2.sorted.sam'
 number = file.ReadSAMFile(filepath)
 
+"""
+It is VITAL that the variable "read_len" is set to be the same as in the script used to create the reads
+"""
+read_len = 100
 
-def get_borders():
+
+def get_border_reads():
+    # This function creates a list with various fields for the reads. The reads which do not map properly (indicated
+    # by the CIGAR string) are returned.
     pos = []
     mapq = []
     chrom = []
@@ -22,52 +29,73 @@ def get_borders():
         cigar.append(file.GetCIGARvalues(i))
         rocigar.append(file.GetField(i, "CIGAR"))
 
-    worst_maps = []
-    for score in enumerate(mapq):
-        if int(score[1]) < 10:
-            # worst_maps.append([score, rocigar[int(score[0])]])
-            worst_maps.append([score[0], score[1]])
-
-    print(worst_maps)
-
-    breakpoint_areas = []
-    r_list = []
-    f_list = []
-    for mapq_score in worst_maps:
-
-        r_temp_list = []
-        f_temp_list = []
-        r_counter = 1
-        f_counter = 0
-
-        while mapq[mapq_score[0] - r_counter + 2] < max(mapq):
-            # backwards
-            current_read = mapq_score[0] - r_counter
-            r_temp_list.append([mapq[current_read], rocigar[current_read], pos[current_read]])
-            r_counter += 1
-
-        r_list.append(r_temp_list[:: - 1])
-        f_list.append([mapq[mapq_score[0]], rocigar[mapq_score[0]], pos[mapq_score[0]]])
-
-        while mapq[mapq_score[0] + f_counter - 1] < max(mapq):
-            # forwards
-            current_read = mapq_score[0] + f_counter
-            f_temp_list.append([mapq[current_read], rocigar[current_read], pos[current_read]])
-            f_counter += 1
-
-        f_list[0].append(f_temp_list)
-        print(r_temp_list)
-
-    # print(r_list[::-1])
-    # print(f_list)
-
     borders = []
-    # for area in f_list:
-    #    print(area)
+    for read in enumerate(rocigar):
+        if read[1] == "100M":
+            pass
+        else:
+            borders.append([chrom[read[0]], pos[read[0]], read_name[read[0]], mapq[read[0]], read[1]])
+
+    # print(borders)
+    return borders
 
 
-# Consider just iterating through the list and
+def get_borders(borders):
+    # This function takes the reads which are suspected to contain rearrangement breakpoints and extracts the CIGAR
+    # strings. Then, the reads are returned BUT with ADJUSTED positions. These positions refer to the last (or first)
+    # mapping read, depending on whether it matched initially or started matching somewhere along its length.
+    bps = []
+    # bps = breakpoints
+    for border in borders:
+        cigar_str = border[4]
+        temp_list = []
+        for character in cigar_str:
+            if character == "M":
+                bps.append(temp_list)
+                break
+            elif character.isalpha() and character != "M":
+                temp_list = ["-"]
+            elif character.isdigit():
+                if not temp_list:
+                    temp_list.append(character)
+                else:
+                    temp_list.append("".join(character))
+
+    # print(bps)
+    # print(len(bps))
+    matching = []
+    for numbers in bps:
+        matching.append("".join(numbers[0:]))
+
+    # print(matching)
+    # print(len(matching))
+
+    breakpoints = []
+    for m in enumerate(matching):
+        if m[1].count("-") == 0:
+            breakpoints.append([
+                borders[m[0]][0], borders[m[0]][1] + int(m[1]), borders[m[0]][2], borders[m[0]][3], borders[m[0]][4]
+            ])
+
+        else:
+            breakpoints.append([
+                borders[m[0]][0], borders[m[0]][1] + read_len + int(m[1]), borders[m[0]][2], borders[m[0]][3],
+                borders[m[0]][4]
+            ])
+
+    print(breakpoints)
+    return breakpoints
+
+
+def write_to_file(breakpoints):
+    my_file = open(file_name, "w")
+    my_file.write(f"chromosome \t position \t read name \t MAPQ \t CIGAR string \n")
+    for bp in breakpoints:
+        my_file.write(f"{bp[0]} \t \t {bp[1]} \t \t {bp[2]} \t \t {bp[3]} \t {bp[4]} \n")
 
 
 if __name__ == "__main__":
-    read_info = get_borders()
+    read_info = get_border_reads()
+    find_breakpoints = get_borders(read_info)
+    file_name = "exp7_2.txt"  # Desired name for output file
+    write_to_file(find_breakpoints)
